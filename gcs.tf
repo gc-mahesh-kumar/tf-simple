3d13c49079b5bf0cc8367cf02c45eecbf186266d
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-09 10:42:04 +0000

    added gcs

--- gcs.tf
+++ gcs.tf
@@ -2,3 +2,8 @@ resource "google_storage_bucket" "default" {
     name = "devops-concepts-01"
     location = "us-central1"
 }
+
+resource "google_storage_bucket" "default-01" {
+    name = "devops-concepts-02"
+    location = "us-central1"
+}
